4bb2a99f07ac4fbe3ddf378b72e32f2b1c83358d
Phil <supersmashphil@gmail.com>
2025-06-18 23:20:55 -0600

    build version and build event update.

--- Source/XmlPatchHelper/XmlPatchPatches.cs
+++ Source/XmlPatchHelper/XmlPatchPatches.cs
@@ -10,44 +10,44 @@ using HarmonyLib;
 
 namespace XmlPatchHelper
 {
-	[StaticConstructorOnStartup]
-	public static class XmlPatchPatches
-	{
-		public const string LogLabel = "[XmlPatchHelper]";
+  [StaticConstructorOnStartup]
+  public static class XmlPatchPatches
+  {
+    public const string LogLabel = "[XmlPatchHelper]";
 
-		public const string PackageId = "smashphil.xmlpatchhelper";
+    public const string PackageId = "smashphil.xmlpatchhelper";
 
-		internal static Texture2D MenuIcon = ContentFinder<Texture2D>.Get("XmlPatchHelper_MenuIcon");
+    internal static Texture2D MenuIcon = ContentFinder<Texture2D>.Get("XmlPatchHelper_MenuIcon");
 
-		internal static ModMetaData MetaData { get; private set; }
+    internal static ModMetaData MetaData { get; private set; }
 
-		internal static Harmony HarmonyInstance { get; private set; }
+    internal static Harmony HarmonyInstance { get; private set; }
 
-		static XmlPatchPatches()
-		{
-			HarmonyInstance = new Harmony("smashphil.xmlpatchhelper");
+    static XmlPatchPatches()
+    {
+      HarmonyInstance = new Harmony("smashphil.xmlpatchhelper");
 
-			Version version = Assembly.GetExecutingAssembly().GetName().Version;
-			MetaData = ModLister.GetActiveModWithIdentifier(PackageId, ignorePostfix: true);
+      Version version = Assembly.GetExecutingAssembly().GetName().Version;
+      MetaData = ModLister.GetActiveModWithIdentifier(PackageId, ignorePostfix: true);
 
-			Log.Message($"<color=orange>{LogLabel}</color> version {MetaData.ModVersion}");
+      Log.Message($"{LogLabel} version {MetaData.ModVersion}");
 
-			XmlPatchConsole.ExcludeField(typeof(PatchOperation), nameof(PatchOperation.sourceFile));
+      XmlPatchConsole.ExcludeField(typeof(PatchOperation), nameof(PatchOperation.sourceFile));
 
-			HarmonyInstance.Patch(AccessTools.Method(typeof(OptionListingUtility), nameof(OptionListingUtility.DrawOptionListing)),
-				prefix: new HarmonyMethod(typeof(XmlPatchPatches),
-				nameof(InsertXmlPatcherButton)));
-		}
+      HarmonyInstance.Patch(
+        AccessTools.Method(typeof(OptionListingUtility),
+          nameof(OptionListingUtility.DrawOptionListing)),
+        prefix: new HarmonyMethod(typeof(XmlPatchPatches),
+          nameof(InsertXmlPatcherButton)));
+    }
 
-		public static void InsertXmlPatcherButton(ref List<ListableOption> optList)
-		{
-			if (optList.Any(opt => opt is ListableOption_WebLink))
-			{
-				optList.Add(new ListableOption_WebLink("XmlPatchHelper".Translate(), delegate ()
-				{
-					Find.WindowStack.Add(new XmlPatchConsole());
-				}, MenuIcon));
-			}
-		}
-	}
+    public static void InsertXmlPatcherButton(ref List<ListableOption> optList)
+    {
+      if (optList.Any(opt => opt is ListableOption_WebLink))
+      {
+        optList.Add(new ListableOption_WebLink("XmlPatchHelper".Translate(),
+          delegate() { Find.WindowStack.Add(new XmlPatchConsole()); }, MenuIcon));
+      }
+    }
+  }
 }
